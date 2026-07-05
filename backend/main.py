7a047214057c714507d3c98ea97dc9339ab8e86d
arushi1106 <jainarushi651@gmail.com>
2026-07-05 21:38:32 +0100

    files updated

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from cv_parser import extract_text_from_pdf
 from ai_resume_parser import parse_resume
 from resume_analyzer import analyze_resume
 from job_matcher import match_resume_to_job
+from resume_optimizer import optimize_resume
 import shutil
 
 app = FastAPI()
@@ -24,6 +25,16 @@ async def job_match(file: UploadFile = File(...), job_description: str = Form(..
     resume_json = parse_resume(text)
     match = match_resume_to_job(resume_json,job_description)
     return {"resume": resume_json,"job_match": match}
+
+@app.post("/optimize-resume")
+async def optimize(file: UploadFile = File(...),job_description: str = Form(...)):
+    file_path = f"uploads/{file.filename}"
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    text = extract_text_from_pdf(file_path)
+    resume_json = parse_resume(text)
+    optimized = optimize_resume(resume_json,job_description)
+    return optimized
 
 if __name__ == "__main__":
     import uvicorn
